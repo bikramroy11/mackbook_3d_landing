7b47abb4a3614606c08daadaf6f67eb279942c31
Bikram Roy <bikramroycs@gmail.com>
2025-10-31 21:12:09 +0530

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
 
 const App = () => {
   return (
     <>
       <main>
         <Navbar/>
+        <Hero/>
+        
       </main>
     </>
   )
